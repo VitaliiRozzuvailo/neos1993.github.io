e370d1ff3f835140604c56551b1d93e31c55ae2d
neos1993 <vrozzuvaylo@gmail.com>
2015-10-16 01:20:25 +0300

    add form

--- js/custom.js
+++ js/custom.js
@@ -10,163 +10,128 @@
 	});
 
 	$(document).ready(function() {
-
-		/* ---------------------------------------------- /*
-		 * Smooth scroll / Scroll To Top
-		/* ---------------------------------------------- */
-
-		$('a[href*=#]').bind("click", function(e){
-           
-			var anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $(anchor.attr('href')).offset().top
-			}, 1000);
-			e.preventDefault();
-		});
-
-		$(window).scroll(function() {
-			if ($(this).scrollTop() > 100) {
-				$('.scroll-up').fadeIn();
-			} else {
-				$('.scroll-up').fadeOut();
-			}
-		});
-
-		/* ---------------------------------------------- /*
-		 * Navbar
-		/* ---------------------------------------------- */
-
-		$('.header').sticky({
-			topSpacing: 0
-		});
-
-		$('body').scrollspy({
-			target: '.navbar-custom',
-			offset: 70
-		})
-
-        
-        /* ---------------------------------------------- /*
-		 * Skills
-        /* ---------------------------------------------- */    
-        //var color = $('#home').css('backgroundColor');
-
-        $('.skills').waypoint(function(){
-            $('.chart').each(function(){
-            $(this).easyPieChart({
-                    size:140,
-                    animate: 2000,
-                    lineCap:'butt',
-                    scaleColor: false,
-                    barColor: '#FF5252',
-                    trackColor: 'transparent',
-                    lineWidth: 10
-                });
-            });
-        },{offset:'80%'});
-        
-        
-        /* ---------------------------------------------- /*
-		 * Quote Rotator
-		/* ---------------------------------------------- */
-       
-			$( function() {
-				/*
-				- how to call the plugin:
-				$( selector ).cbpQTRotator( [options] );
-				- options:
-				{
-					// default transition speed (ms)
-					speed : 700,
-					// default transition easing
-					easing : 'ease',
-					// rotator interval (ms)
-					interval : 8000
-				}
-				- destroy:
-				$( selector ).cbpQTRotator( 'destroy' );
-				*/
-
-				$( '#cbp-qtrotator' ).cbpQTRotator();
-
-			} );
 		
-        
-		/* ---------------------------------------------- /*
-		 * Home BG
-		/* ---------------------------------------------- */
-
-		$(".screen-height").height($(window).height());
-
-		$(window).resize(function(){
-			$(".screen-height").height($(window).height());
-		});
-
-		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-			$('#home').css({'background-attachment': 'scroll'});
+	/* ---------------------------------------------- /*
+	 * Input text to frm
+	/* ---------------------------------------------- */   
+	$('textarea').blur(function() {
+	  $('#contact textarea').each(function() {
+		$this = $(this);
+		if (this.value != '') {
+		  $this.addClass('focused');
+		  $('textarea + label + span').css({
+			'opacity': 1
+		  });
 		} else {
-			$('#home').parallax('50%', 0.1);
+		  $this.removeClass('focused');
+		  $('textarea + label + span').css({
+			'opacity': 0
+		  });
 		}
+	  });
+	});
 
+	$('#contact .field:first-child input').blur(function() {
+	  $('#contact .field:first-child input').each(function() {
+		$this = $(this);
+		if (this.value != '') {
+		  $this.addClass('focused');
+		  $('.field:first-child input + label + span').css({
+			'opacity': 1
+		  });
+		} else {
+		  $this.removeClass('focused');
+		  $('.field:first-child input + label + span').css({
+			'opacity': 0
+		  });
+		}
+	  });
+	});
 
-		/* ---------------------------------------------- /*
-		 * WOW Animation When You Scroll
-		/* ---------------------------------------------- */
+	$('#contact .field:nth-child(2) input').blur(function() {
+	  $('#contact .field:nth-child(2) input').each(function() {
+		$this = $(this);
+		if (this.value != '') {
+		  $this.addClass('focused');
+		  $('.field:nth-child(2) input + label + span').css({
+			'opacity': 1
+		  });
+		} else {
+		  $this.removeClass('focused');
+		  $('.field:nth-child(2) input + label + span').css({
+			'opacity': 0
+		  });
+		}
+	  });
+	});
+	
+	/* ---------------------------------------------- /*
+	 * Skills
+	/* ---------------------------------------------- */    
+	//var color = $('#home').css('backgroundColor');
 
-		wow = new WOW({
-			mobile: false
+	$('.skills').waypoint(function(){
+		$('.chart').each(function(){
+		$(this).easyPieChart({
+				size:140,
+				animate: 2000,
+				lineCap:'butt',
+				scaleColor: false,
+				barColor: '#f05f40',
+				trackColor: 'transparent',
+				lineWidth: 10
+			});
 		});
-		wow.init();
+	},{offset:'80%'});
+	
+	/* ---------------------------------------------- /*
+	 * E-mail validation
+	/* ---------------------------------------------- */
 
+	function isValidEmailAddress(emailAddress) {
+		var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+		return pattern.test(emailAddress);
+	};
 
-		/* ---------------------------------------------- /*
-		 * E-mail validation
-		/* ---------------------------------------------- */
+	/* ---------------------------------------------- /*
+	 * Contact form ajax
+	/* ---------------------------------------------- */
 
-		function isValidEmailAddress(emailAddress) {
-			var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-			return pattern.test(emailAddress);
+	$('#contact-form').submit(function(e) {
+
+		e.preventDefault();
+
+		var c_name = $('#c_name').val();
+		var c_email = $('#c_email').val();
+		var c_message = $('#c_message ').val();
+		var response = $('#contact-form .ajax-response');
+		
+		var formData = {
+			'name'       : c_name,
+			'email'      : c_email,
+			'message'    : c_message
 		};
 
-		/* ---------------------------------------------- /*
-		 * Contact form ajax
-		/* ---------------------------------------------- */
+		if (( c_name== '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email) )) {
+			response.fadeIn(500);
+			response.html('<i class="fa fa-warning"></i> Please fix the errors and try again.');
+		}
 
-		$('#contact-form').submit(function(e) {
-
-			e.preventDefault();
-
-			var c_name = $('#c_name').val();
-			var c_email = $('#c_email').val();
-			var c_message = $('#c_message ').val();
-			var response = $('#contact-form .ajax-response');
-			
-			var formData = {
-				'name'       : c_name,
-				'email'      : c_email,
-				'message'    : c_message
-			};
-
-			if (( c_name== '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email) )) {
-				response.fadeIn(500);
-				response.html('<i class="fa fa-warning"></i> Please fix the errors and try again.');
-			}
-
-			else {
-					 $.ajax({
-							type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-							url         : 'assets/php/contact.php', // the url where we want to POST
-							data        : formData, // our data object
-							dataType    : 'json', // what type of data do we expect back from the server
-							encode      : true,
-							success		: function(res){
-											var ret = $.parseJSON(JSON.stringify(res));
-											response.html(ret.message).fadeIn(500);
-							}
-						});
-				}           
-            	return false;
-			});
+		else {
+				 $.ajax({
+						type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+						url         : 'assets/php/contact.php', // the url where we want to POST
+						data        : formData, // our data object
+						dataType    : 'json', // what type of data do we expect back from the server
+						encode      : true,
+						success		: function(res){
+										var ret = $.parseJSON(JSON.stringify(res));
+										response.html(ret.message).fadeIn(500);
+						}
+					});
+			}           
+			return false;
+		});
 
 	});
 
